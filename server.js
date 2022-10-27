2137089127183791acbdab5402d3ae42cc075bac
Payton Morris <payton@morrisandmorris.com>
2022-10-27 14:30:32 -0600

    done

--- server.js
+++ server.js
@@ -5,5 +5,6 @@ http.createServer(function(request, response) {
    response.write("<h1>Hello, Node.js!</h1>");    
    response.write("<h2>Payton Morris - s31k237</h2> \
 		   <p>Running node server on port 3023</p>");
+   response.write("<a href='https://github.com/spobozo/NODE'>Code on githum</a>");
    response.end();
 }).listen(3023);
