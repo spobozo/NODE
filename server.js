63294ccb6e29e843036aca805591d23068c72351
Payton Morris <payton@morrisandmorris.com>
2022-10-27 14:05:57 -0600

    done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 http.createServer(function(request, response) {    
    response.writeHead(200, {"Content-Type": "text/html"});
    response.write("<h1>Hello, Node.js!</h1>");    
-   response.write("<h2>Daniel DeFrance - h48c334</h2> \
-		   <p>Running node server on port 3022</p>");
+   response.write("<h2>Payton Morris - s31k237</h2> \
+		   <p>Running node server on port 3023</p>");
    response.end();
-}).listen(3022);
+}).listen(3023);
